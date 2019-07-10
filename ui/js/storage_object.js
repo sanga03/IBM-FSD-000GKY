75bf7673ef85df93fe6e6732cd3d5f2e7429b2d9
Sangamesh <sangabi1@in.ibm.com>
2019-07-10 17:43:19 +0530

    change to sort

--- ui/js/storage_object.js
+++ ui/js/storage_object.js
@@ -22,16 +22,82 @@ class data{
        }
        else{
              Object.keys(localStorage).forEach((key)=>{
-               let item = localStorage.getItem(key);
+                 let item = localStorage.getItem(key);
                let dat = JSON.parse(item);
                 console.log(dat);
-                rows +=`<tr>    <td> ${dat.name} </td>           <td> ${key}</td>            <td> ${dat.org} </td></tr>`
+                rows +=`<tr>    <td> ${dat.name} </td>           <td> ${key}</td>            <td> ${dat.org} </td>
+                <td><a href='#' onclick="deleteEmployee('${key}')">Delete</a></td>
+                    <td><a href='#' onclick="updateEmployee('${key}')">Update</a></td>
+                </tr>`
              }) ;
             }
-let table=`<table> <tr><th> Name </th><th> email </th><th> Organizarion </th></tr>${rows}</table>`
+let table=`  <table> <tr><th> Name <input type="button" value="ascend" onclick="ascend('name')"/> <input type="button" value="desc" onclick="descend("name")/>
+</th><th> email <input type="button" value="ascend" onclick="ascend('email')"/> <input type="button" value="desc" onclick="descend("email")/> </th>
+<th> Organizarion<input type="button" value="ascend" onclick="ascend('org')"/> <input type="button" value="desc" onclick="descend("org")/> </th></tr>${rows}</table>`
 document.getElementById('table-div').innerHTML=table;
       
    }
+}
+function ascend(val){
+ 
+    let all_Emp_data=[];  
+    Object.keys(localStorage).forEach((key)=>{
+            all_Emp_data.push(JSON.parse(localStorage.getItem(key)));
+        });
+        
+// console.log(all_Emp_data);
+let sorted_emp=[];
+    switch (val) {
+       
+    case "name":
+          sorted_emp.push(all_Emp_data.sort((e1,e2)=>{
+                if(e1.name >e2.name){return 1}else if(e1.name<e2.name) return -1; else return 0;  
+            }));
+            break;
+    case "email":
+        sorted_emp.push( all_Emp_data.sort((e1,e2)=>{
+                if(e1.email >e2.email){return 1}else if(e1.email<e2.email) return -1; else return 0;  
+            }));
+            break;
+    case "org":
+        sorted_emp.push( all_Emp_data.sort((e1,e2)=>{
+            if(e1.org >e2.org){return 1}else if(e1.org<e2.org) return -1; else return 0;  
+        }));
+        break;
+        default:
+            break;
+    }
+    console.log("sorted",sorted_emp[0]);
+    localStorage.clear();
+    for(emp of sorted_emp[0]){
+        console.log("emp",emp);
+        localStorage.setItem(emp.email,JSON.stringify(emp));
+    }
+    data.display();
+}
+
+const deleteEmployee=(key)=>{
+    localStorage.removeItem(key);
+    data.display();
+}
+const updateEmployee=(key)=>{
+
+//    let ind= Object.keys(localStorage).find((keyz)=>{
+//         return keyz==key;
+//     });
+//     console.log(ind);
+    let emptemp;
+   Object.keys(localStorage).forEach((keyz)=>{
+        if(key==keyz){
+            // let tt = 
+            emptemp=JSON.parse(localStorage.getItem(keyz));
+        }
+   });
+   document.getElementById("name").value=emptemp.name;
+   document.getElementById("org").value=emptemp.org;
+   document.getElementById("email").value=emptemp.email;
+   console.log(emptemp);
+
 }
 const onload=()=>{
     data.display();
