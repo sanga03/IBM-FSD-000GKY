5029ebafccdb1d967f0395e6c8cc57976a27a318
Sangamesh <sangabi1@in.ibm.com>
2019-07-11 09:07:23 +0530

    working ascend and descend

--- ui/js/storage_object.js
+++ ui/js/storage_object.js
@@ -31,13 +31,45 @@ class data{
                 </tr>`
              }) ;
             }
-let table=`  <table> <tr><th> Name <input type="button" value="ascend" onclick="ascend('name')"/> <input type="button" value="desc" onclick="descend("name")/>
-</th><th> email <input type="button" value="ascend" onclick="ascend('email')"/> <input type="button" value="desc" onclick="descend("email")/> </th>
-<th> Organizarion<input type="button" value="ascend" onclick="ascend('org')"/> <input type="button" value="desc" onclick="descend("org")/> </th></tr>${rows}</table>`
+let table=`  <table> <tr><th> Name <input type="button" value="ascend" onclick="ascend('name')"/> <input type="button" value="desc" onclick="descend('name')"/>
+</th><th> email <input type="button" value="ascend" onclick="ascend('email')"/> <input type="button" value="desc" onclick="descend('email')"/> </th>
+<th> Organizarion<input type="button" value="ascend" onclick="ascend('org')"/> <input type="button" value="desc" onclick="descend('org')"/> </th></tr>${rows}</table>`
 document.getElementById('table-div').innerHTML=table;
       
    }
 }
+function descend(val){
+    let all_Emp_data=[];  
+    Object.keys(localStorage).forEach((key)=>{
+            all_Emp_data.push(JSON.parse(localStorage.getItem(key)));
+        });
+        
+// console.log(all_Emp_data);
+let sorted_emp=[];
+    switch (val) {
+       
+    case "name":
+          sorted_emp.push(all_Emp_data.sort((e1,e2)=>{
+                if(e1.name >e2.name){return -1}else if(e1.name<e2.name) return 1; else return 0;  
+            }));
+            break;
+    case "email":
+        sorted_emp.push( all_Emp_data.sort((e1,e2)=>{
+                if(e1.email >e2.email){return -1}else if(e1.email<e2.email) return 1; else return 0;  
+            }));
+            break;
+    case "org":
+        sorted_emp.push( all_Emp_data.sort((e1,e2)=>{
+            if(e1.org >e2.org){return -1}else if(e1.org<e2.org) return 1; else return 0;  
+        }));
+        break;
+        default:
+            break;
+    }
+    console.log("sorted",sorted_emp[0]);
+    table_display(sorted_emp[0]);
+}
+
 function ascend(val){
  
     let all_Emp_data=[];  
@@ -68,14 +100,30 @@ let sorted_emp=[];
             break;
     }
     console.log("sorted",sorted_emp[0]);
-    localStorage.clear();
-    for(emp of sorted_emp[0]){
-        console.log("emp",emp);
-        localStorage.setItem(emp.email,JSON.stringify(emp));
-    }
-    data.display();
+    table_display(sorted_emp[0]);
+    // localStorage.clear();
+    // for(emp of sorted_emp[0]){
+    //     console.log("emp",emp);
+    //     localStorage.setItem(emp.email,JSON.stringify(emp));
+    // }
+    // data.display();
 }
-
+const table_display = (emp)=>{
+    rows = '';
+    document.getElementById('table-div').innerHTML='';
+    for (dat of emp.entries()){
+        dat = dat[1];
+        console.log("dat",dat);
+        rows +=`<tr>    <td> ${dat.name} </td>           <td> ${dat.email}</td>            <td> ${dat.org} </td>
+                <td><a href='#' onclick="deleteEmployee('${dat.email}')">Delete</a></td>
+                    <td><a href='#' onclick="updateEmployee('${dat.email}')">Update</a></td>
+                </tr>`
+    }
+    let table=`  <table> <tr><th> Name <input type="button" value="ascend" onclick="ascend('name')"/> <input type="button" value="desc" onclick="descend('name')"/>
+</th><th> email <input type="button" value="ascend" onclick="ascend('email')"/> <input type="button" value="desc" onclick="descend('email')"/> </th>
+<th> Organizarion<input type="button" value="ascend" onclick="ascend('org')"/> <input type="button" value="desc" onclick="descend('org')"/> </th></tr>${rows}</table>`
+document.getElementById('table-div').innerHTML=table;
+}
 const deleteEmployee=(key)=>{
     localStorage.removeItem(key);
     data.display();
