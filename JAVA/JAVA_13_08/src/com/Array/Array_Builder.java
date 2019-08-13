5b82753ceffbd6adb405f23fe55f9ccbd6fca262
Sangamesh <sangabi1@in.ibm.com>
2019-08-13 15:10:00 +0530

    cha

--- JAVA/JAVA_13_08/src/com/Array/Array_Builder.java
+++ JAVA/JAVA_13_08/src/com/Array/Array_Builder.java
@@ -1,19 +1,49 @@
 package com.Array;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Stack;
+
 public class Array_Builder {
 private char arr[];
-
+Stack<Integer> stk ;
+int len;
 public Array_Builder() {
-	
+	stk = new Stack<Integer>();
 }
-public Array_Builder(int ln) {
+public Array_Builder(int ln,int len) {
 	arr = new char[ln];
+	this.len =len;
+
 }
 public char[] build(char ch) {
 	for(int i=0;i<arr.length;i++) {
 		arr[i]=(char)(ch+i);
 	}
 	return arr;
+}
+public int[] sortElement(int[] arr){
+	Arrays.sort(arr);
+	return arr;
+}
+public int findElement(int[] arr,int item){
+	return (Arrays.binarySearch(arr, item));
+}
+public boolean pushItem(int item){
+	if(stk.size() < len){
+		System.out.println("Over flow");
+		return false;
+	}
+	else
+	stk.push(item);
+	return true;
+}
+public int popItem(){
+	if(stk.size()<1){
+		System.out.println("Under flow");
+		return -1;
+	}
+	return stk.pop();
 }
 
 }
--- JAVA/JAVA_13_08/src/com/Array/Main_Arr.java
+++ JAVA/JAVA_13_08/src/com/Array/Main_Arr.java
@@ -1,11 +1,22 @@
 package com.Array;
-
+import com.Array.Array_Builder;;
 public class Main_Arr {
 
 	public static void main(String[] args) {
-		Array_Builder array_Builder = new Array_Builder(24);
+		Array_Builder array_Builder = new Array_Builder(24,4);
 		System.out.println(array_Builder.build('('));
+		int arr[]={
+			123,123,1,1,2,1,2,1,3,123,123,-123,13,123
+		};
+		arr =array_Builder.sortElement(arr);
+		for(int a:arr){
+			System.out.println(a);
+		}
 
+		if(array_Builder.pushItem(10)) System.out.println("Success"); else System.out.println("failure");;
+		if(array_Builder.pushItem(120)) System.out.println("Success"); else System.out.println("failure");;
+		if(array_Builder.pushItem(110)) System.out.println("Success"); else System.out.println("failure");;
+		if(array_Builder.pushItem(102)) System.out.println("Success"); else System.out.println("failure");;
 	}
 
 }
