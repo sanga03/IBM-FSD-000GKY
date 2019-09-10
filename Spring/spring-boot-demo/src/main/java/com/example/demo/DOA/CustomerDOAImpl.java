6714f7fa4ab2b8d5b29121bee5be2b8d7fb0d2ec
sangu <b4ibmjava20@iiht.tech>
2019-09-10 17:49:11 +0530

    updated BOOT CRUD

--- Spring/spring-boot-demo/src/main/java/com/example/demo/DOA/CustomerDOAImpl.java
+++ Spring/spring-boot-demo/src/main/java/com/example/demo/DOA/CustomerDOAImpl.java
@@ -48,6 +48,8 @@ public class CustomerDOAImpl implements CustomerDOA {
 
 	@Transactional
 	public void deleteCustomer(int id) {
+		session = em.unwrap(Session.class);
+		
 		Customer c = (Customer) session.load(Customer.class, id);
 		System.out.println(c);
 		session.beginTransaction();
@@ -57,6 +59,8 @@ public class CustomerDOAImpl implements CustomerDOA {
 	}
 
 	public void updateCustomer(Customer ciCustomer) {
+		session = em.unwrap(Session.class);
+		
 		System.out.println(ciCustomer.getId());
 		session.beginTransaction();
 		session.merge(ciCustomer);
@@ -81,6 +85,8 @@ public class CustomerDOAImpl implements CustomerDOA {
 	}
 
 	public Customer getByID(int id) {
+		session = em.unwrap(Session.class);
+		
 		Query rc = session.createQuery("from Customer where id =" + id);
 		List<Customer> lc = rc.getResultList();
 		return lc.get(0);
--- Spring/spring-boot-demo/src/main/java/com/example/demo/controller/HomeController.java
+++ Spring/spring-boot-demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -3,7 +3,12 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,15 +20,37 @@ import com.example.demo.Services.CustomerServices;
 public class HomeController {
 
 	@Autowired
-	private CustomerServices cservice;
+	private CustomerServices customerServices;
 
 	@GetMapping("/index")
 	public String Home() {
 		return "Hello";
 	}
 
-	@GetMapping("/customer")
+	@GetMapping("/customer")	
 	public List<Customer> getCustomer() {
-		return cservice.getCustomers();
+		return customerServices.getCustomers();
+	}
+	@PostMapping("/customer")
+	public Customer addCustomers(@RequestBody Customer customer) {
+		
+		customerServices.updateCustomer(customer);
+		return customer;
+		
+	}
+	@DeleteMapping("/customer/{id}")
+	public boolean deleteCustomer(@PathVariable int id) {
+	customerServices.deleteCustomer(id);
+		return true;
+		
+	}
+	@PutMapping("/customer")
+	public boolean updateCustomer(@RequestBody Customer customer) {
+		customerServices.updateCustomer(customer);
+		return true;
+	}
+	@GetMapping("/customer/{id}")
+	public Customer getById(@PathVariable int id) {
+		return customerServices.getByID(id);
 	}
 }
